ecc95dcfbb3bf4f82b5ceda1ef95ad472f132ad7
Eslam-Alawy <59186273+Eslam-Alawy@users.noreply.github.com>
2021-08-28 04:59:51 +0200

    Update b.py

--- b.py
+++ b.py
@@ -42,10 +42,10 @@ def doitnow():
     while k < howmany:
      #os.system('youtube-dl -f best --console-title --newline --no-warnings --no-part -o "{1}" --user-agent "Mozilla / 5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko / 20100101 Firefox / 65.0" -v --newline {0}'.format(urlar[k],vidnamear[k]))
      os.system('ffmpeg -i {0} -i logo.png  -filter_complex "overlay=5:5:format=auto,format=yuv420p,ass={1}" {2}'.format(vidnamear[k],vidsub[k],vidnameoutar[k]))
-     #os.system('rm -f {0} && rm -f {1}'.format(vidsub[k],vidnamear[k]))
+     os.system('rm -f {0} && rm -f {1}'.format(vidsub[k],vidnamear[k]))
      os.system('megaput {0} --config a.megarc'.format(vidnameoutar[k]))
      os.system('echo "{0} Uploaded Successfully."'.format(vidnameoutar[k]))
-     #os.system('rm -f {0}'.format(vidnameoutar[k]))
+     os.system('rm -f {0}'.format(vidnameoutar[k]))
      #last way
      #os.system('touch {0}.py'.format(vidnameoutar[k]))
      #os.system('printf "import sys\nimport os\nsys.path.append(os.path.abspath(\'/usr/local/lib/python2.7/dist-packages\'))\nfrom mega import Mega\nmega = Mega()\nm = mega.login(\'discinema_1@getnada.com\',\'Lord7418529630\')\nfile = m.upload(\'{0}\')\nos.system(\'rm {0}.py && rm {0} && rm {1} && rm {2}\')" > {0}.py'.format(vidnameoutar[k],vidsub[k],vidnamear[k]))
